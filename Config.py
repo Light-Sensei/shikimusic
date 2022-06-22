4ec679e2e3b689a25060dac18ab6db7250446b75
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-06-22 20:57:07 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "8714251"))
     API_HASH = os.environ.get("API_HASH", "50c97a11b622575c5b9441b1062f601a")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5276474965:AAFGJNgQEWJH1qQ8RlHBNAM9e3aJH7ke18c")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOG0BuxbW5PElazMuhK6xZtidhvlbusQdzxLKD0vziLhhiKqwzN5t6DIK_AEbe7fdkSlkM0RxJlVjj0bNBmeGUDlIJpFiolEWB1vra0Sqhn77Owg6xhnibD2qBU4m6anb7hVxtFnaLnX6vPixTYXftI__RWG-W09iDS8eP76LZfipGMIuRIWiytnEUIH7K4W17TvbjdpG4_QIzC0emWsu5E0PFfqiGIB8yqZFWKRkNqk3gHUg2YzrXNbSf4BGPdjnnCZaHsRgIS9Q2ub5oOlUxBfMmFiDZ1Uij6z-Tx9zFlxnEcQS23wmgU4fR7QLSFHuK3vH4UGCNXsWedOQBuB32SQwTzc=")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "BoaHancock_Robot")
     SUPPORT = os.environ.get("SUPPORT", "BoaHancock_Support")
